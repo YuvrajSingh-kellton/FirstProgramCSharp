00e780615c9e0b2e60797a77e43d74833005be6b
YuvrajSingh-kellton <105622006+YuvrajSingh-kellton@users.noreply.github.com>
2022-08-10 09:09:33 +0530

    Initial Commit

---
+++ Firstoprogram/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace HelloWorld
+{
+    //A simple program to display the words Hello World
+    public class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.Read();
+        }
+    }
+}
---
+++ Firstoprogram/ProgramHelpers.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace HelloWorld
+{
+    internal static class ProgramHelpers
+    {
+        static void Main(string[] args)
+    }
+}
